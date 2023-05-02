5eca2d60afc282b1412a8ac42bfcf5d973e021e2
Ogechi Ogbonna <ogechi.ogbonna@yahoo.com>
2023-05-02 00:05:05 -0700

    My Ops Challenge Redo

---
+++ 2010C3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Ogechi Ogbonna
+# Date of latest revision: 5/0123
+# Purpose: Print a string to the terminal
+#Main
+
+sudo last /var/log/wtmp
+
+echo "This is login history redo"
+
+sudo last -3
+
+#End
